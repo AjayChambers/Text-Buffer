d3777b054f0f1b87280eaf91a9a8ba077a796b94
Andrew Chambers <w3dojo@gmail.com>
2025-11-08 14:36:55 -0800

    Preparing to write the final class Text::Buffer using buffer.hpp & TextBuffer.cpp

--- src/text-buffer.cpp
+++ src/text-buffer.cpp
@@ -1,11 +1,13 @@
+#include "coordinate.hpp"
 #include <text/buffer.hpp>
 #include <text/position.hpp>
 #include <utils/err.hpp>
+#include <utility>
 
 using namespace Text_Buffer;
 
 namespace Text {
 
-//
+
 
 }  // Text
