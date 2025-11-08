26a2cc5132dc35d78691ed26a622ef59d07142e1
Andrew Chambers <w3dojo@gmail.com>
2025-11-08 14:34:20 -0800

    Adding Position class. Nearly finished.

--- include/text/position.hpp
+++ include/text/position.hpp
@@ -7,22 +7,51 @@
 #include <concepts>
 #include <string>
 #include <string_view>
+#include <utility>
+#include <utils/err.hpp>
 
-namespace Text
+namespace Text {
+
+
+struct Coordinates
 {
+    const Coordinate row;
+    const Coordinate col;
+};
 
-class Position {
-  Coordinate row;
-  Coordinate col;
 
- public:
-  Position();
 
-  template <std::unsigned_integral T>
-  Position(T rowNumber, T columnNumber)
-  : row(rowNumber)
-  , col(columnNumber)
-  {}
+class Position
+{
+    Coordinate row;  /// @private
+    Coordinate col;  /// @private
+
+  public:
+    Position();
+    Position(size_t rowNum);
+    Position(size_t rowNum, size_t colNum);
+    Position(Position &) noexcept  = default;               /// Copy Ctor
+    Position(Position &&) noexcept = default;               /// Move Ctors
+
+    Position &operator = (const Position &)     = default;  /// Copy Assignment Ops
+    Position &operator = (Position &&) noexcept = default;  /// Move Assignment Op
+
+    Coordinates getCoordinates() noexcept;
+    Coordinate  getRow() noexcept;
+    Coordinate  getCol() noexcept;
+
+    void setPosition(size_t rowNum, size_t colNum);
+    void setRow(size_t rowNum);
+    void setCol(size_t colNum);
+
+    Position &operator ++ () noexcept;
+    Position &operator ++ (int) noexcept;
+    Position &operator -- () noexcept;
+    Position &operator -- (int) noexcept;
+
+    friend bool operator == (const Position &lhs, const Position &rhs) noexcept;
+    friend std::strong_ordering operator <=> (const Position &lhs, const Position &rhs)
+      noexcept;
 };
 
 };
